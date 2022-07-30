688f077322847433430c4f35b57623d5d4597b22
Ayush Purohit <ayushpurohit140@gmail.com>
2022-07-30 21:59:36 +0530

    commits made

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.post("/",function(req,res){
             })
     })
 })
-app.listen(3000,function(){
-    console.log("server is running at port 3000");
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on ${PORT}`));
+
